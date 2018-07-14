041715dd53a29d11ad8da44635d4751cd11f9013
i3wgnit <i3wgnit@gmail.com>
2018-07-14 13:35:06 -0400

    Did some more work :-)

--- src/smart.rkt
+++ src/smart.rkt
@@ -1,16 +1,108 @@
 #lang racket
 (require "evmasm.rkt")
 (define prog
-  '(;; ===
+  `(;; ===
     ;; Constants
     ;; ===
 
-    (def codePointer 0)
-    (def tapePointer 32)
-    (def outLength 64)
+    (def codeLength 0)
+    (def codePointer 32)
 
-    (calldataload 0)
+    (def tapeLength 64)
+    (def tapePointer 96)
+
+    (def outLength 128)
+    (def codeP_off 160)
+
+    ;; ===
+    ;; Constructor
+    ;; ===
+
+    ;; ===
+    ;; Initialization
+    ;; ===
+
+    ;; ===
+    ;; Main loop
+    ;; ===
+    (dest mainLoop:start)
+
+    ;; ===
+    ;; Check if character is valid
+    ;; ===
+    (mload codePointer)
+    (mstore codePointer (add 1 (dup1)))
+    (and #xff (mload (add codeP_off)))
+
+    (jumpi label:deploy (iszero (dup1))) ;; should check with code length instead
+
+    (jumpi label:+ (eq ,(char->integer #\+) (dup1)))
+    (jumpi label:- (eq ,(char->integer #\-) (dup1)))
+    (jumpi label:> (eq ,(char->integer #\>) (dup1)))
+    (jumpi label:< (eq ,(char->integer #\<) (dup1)))
+    (jumpi label:A (eq ,(char->integer #\[) (dup1)))
+    (jumpi label:B (eq ,(char->integer #\]) (dup1)))
+    (jumpi label:. (eq ,(char->integer #\.)))
+
+    (jump mainLoop:start)
+
+    ;; ===
+    ;; BF Operators
+    ;; ===
+
+    (dest label:+)
+    (pop)
+    (mload tapePointer)
+    (add (and #xff (mload (dup2))) 1)
+    (mstore8 (swap1))
+    (jump mainLoop:start)
+
+    (dest label:-)
+    (pop)
+    (mload tapePointer)
+    (sub (and #xff (mload (dup2))) 1)
+    (mstore8 (swap1))
+    (jump mainLoop:start)
+
+    (dest label:>)
+    (pop)
+    (add (mload tapePointer) 1)
     (mstore tapePointer)
+    (jump mainLoop:start)
+
+    (dest label:<)
+    (pop)
+    (sub (mload tapePointer) 1)
+    (mstore tapePointer)
+    (jump mainLoop:start)
+
+    (dest label:A)
+    (pop)
+    (mload codePointer)
+    (jump mainLoop:start)
+
+    (dest label:B)
+    (pop)
+    (jumpi B:loop (mload tapePointer))
+    (pop)
+    (jump mainLoop:start)
+
+    (dest B:loop)
+    (mstore codePointer)
+    (jump mainLoop:start)
+
+    (dest label:.)
+    (and #xff (mload (mload tapePointer)))
+    (mload outLength)
+    (mstore outLength (add 1 (dup1)))
+    (mstore8 (add (+ codeP_off (+ (mload codeLength) (mload tapeLength)))))
+    (jump mainLoop:start)
+
+
+
+
+
+    ;; (jumpi mainLoop:start ) if end of file
     ))
 
 (evm-assemble prog)
